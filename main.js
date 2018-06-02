f2142cfb19bd1743a58c6573ccb92b8443443907
Ioana Vladau <ioan0414@stud.kea.dk>
2018-06-03 00:45:42 +0200

    added ordinal numbers

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 "use strict";
 
+//on DOMLoad, call the function that fetches the JSON
 document.addEventListener("DOMContentLoaded", ()=>{
     getData("https://kea-alt-del.dk/kata-distortion/");
 });
 
+//function that fetches the JSON once X seconds
 function getData(link) {
     // console.log(link);
    setInterval(()=>{
@@ -15,13 +17,27 @@ function getData(link) {
    }, 2000);
 }
 
+// function that gets the queue number, that contains another function that populates the page
 function getQueueNumber(data){
     let queueTemplate = document.querySelector('.queueTemplate').content;
     let clone = queueTemplate.cloneNode(true);
+    document.querySelector("#queue").appendChild(clone);
+
+    //function populatePage that receives the same parameter as getQueueNumber
+    populatePage(data);
+}
+
+function populatePage(data){
     document.querySelector("#queue-bar").style.width = `${data.inQueue}vw`;
     document.querySelector("#people-in-queue").setAttribute("value", data.inQueue);
-
-
+    document.querySelector("#queue-number").innerHTML = data.inQueue;
+    document.querySelector(".queue-number-ending").innerHTML = getOrdinal(data.inQueue);
     console.log(data.inQueue);
-    document.querySelector("#queue").appendChild(clone);
 }
+
+function getOrdinal(n) {
+    let s = ["th","st","nd","rd"];
+    let v = n % 100;
+    return s[(v-20)%10]||s[v]||s[0];
+    // return n+(s[(v-20)%10]||s[v]||s[0]);
+ }
