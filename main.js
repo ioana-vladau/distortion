09127597d6b856f4be28ebd8c854c0a1b6c5db00
Ioana Vladau <ioan0414@stud.kea.dk>
2018-06-03 02:16:12 +0200

    3 versions of it, might use more styling

--- main.js
+++ main.js
@@ -14,25 +14,40 @@ function getData(link) {
         return response.json();
     })
     .then(getQueueNumber) 
-   }, 2000);
+   }, 3000);
 }
 
 // function that gets the queue number, that contains another function that populates the page
-function getQueueNumber(data){
-    let queueTemplate = document.querySelector('.queueTemplate').content;
-    let clone = queueTemplate.cloneNode(true);
-    document.querySelector("#queue").appendChild(clone);
+/*function getQueueNumber(data){
+    // IN CASE WE NEED TO HAVE A TEMPLATE, USE THIS
+
+    // let queueTemplate = document.querySelector('.queueTemplate').content;
+    // let clone = queueTemplate.cloneNode(true);
+    // document.querySelector("#queue").appendChild(clone);
 
     //function populatePage that receives the same parameter as getQueueNumber
     populatePage(data);
-}
+}*/
 
-function populatePage(data){
+function getQueueNumber(data){
     document.querySelector("#queue-bar").style.width = `${data.inQueue}vw`;
+    document.querySelector("#total-people").innerHTML="";
+
+    let arrayDiv = new Array();
+    let last = false;
+
+    for(let i=0; i<data.inQueue; i++){
+        let elementNumber = arrayDiv[i];
+        arrayDiv[i] = document.createElement('img');
+        arrayDiv[i].className = 'person-in-queue';
+        // setting the class-name of the created elements:    
+        document.querySelector("#total-people").appendChild(arrayDiv[i]);
+    }
+
     document.querySelector("#people-in-queue").setAttribute("value", data.inQueue);
     document.querySelector("#queue-number").innerHTML = data.inQueue;
     document.querySelector(".queue-number-ending").innerHTML = getOrdinal(data.inQueue);
-    console.log(data.inQueue);
+    // console.log(data.inQueue);
 }
 
 function getOrdinal(n) {
